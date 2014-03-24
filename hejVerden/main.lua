abb3da0f0f5fad28bc0802542012feaa136d58d9
asathoor <per@multimusen.dk>
2014-03-24 09:01:14 +0100

    Corona Lessons first commit

---
+++ hejVerden/main.lua
@@ -0,0 +1,5 @@
+-- Corona Hej Verden 
+-- eksempel baseret paa http://developer.coronalabs.com/resources/corona-quick-start-guide 
+
+myText = display.newText( "Hej Verden!", 200, 40, native.systemFont, 36 )
+myText:setTextColor( 255,0,0 )
