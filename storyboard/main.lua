e4a5bfda929d8a4675eb35e4c5011f1f501e393a
asathoor <per@multimusen.dk>
2014-03-30 21:07:49 +0200

    Storyboard source url

--- storyboard/main.lua
+++ storyboard/main.lua
@@ -1,3 +1,4 @@
 -- main.lua
+-- based on http://www.develephant.net/a-simple-storyboard-framework-for-corona-sdk-part-1/
 local storyboard = require( "storyboard" )
 storyboard.gotoScene( "scene1" )
