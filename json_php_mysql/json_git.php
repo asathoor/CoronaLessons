dbb569fb5e181ae56035a4eea2de86f4e87939b6
asathoor <per@multimusen.dk>
2014-04-07 11:56:44 +0200

    json,php,mysql

--- json_php_mysql/json_git.php
+++ json_php_mysql/json_git.php
@@ -1,14 +1,15 @@
 <?php 
+
 /*
 PHP JSON ENCODE MYSQL
 Eksempel - resultatet kan bruges i Lua.
 */
 
 // CONNECT TO THE DATABASE
-	$DB_NAME = 'DATABASE_NAME';
-	$DB_HOST = 'DATABASE_HOST';
-	$DB_USER = 'DATABASE_USER';
-	$DB_PASS = 'DATABASE_PASSWORD';
+	$DB_NAME = 'XXXX';
+	$DB_HOST = 'localhost';
+	$DB_USER = 'XXXX';
+	$DB_PASS = 'XXXX';
 	
 	$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 	
@@ -17,30 +18,19 @@ Eksempel - resultatet kan bruges i Lua.
 		exit();
 	}
 
-// A QUICK QUERY ON A FAKE USER TABLE
-	$query = "SELECT * FROM `users` WHERE `status`='bonkers'";
+// utf-8
+mysqli_query($mysqli, 'SET CHARACTER SET utf8');
+
+// A QUICK QUERY
+	$query = "SELECT * FROM `Albums`";
 	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
-
-/*
-Ikke noedvendig i denne oevelse
-
-// GOING THROUGH THE DATA
-	if($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			echo stripslashes($row['username']);	
-		}
-	}
-	else {
-		echo 'NO RESULTS';	
-	}
-*/	
 
 // JSON CODE
 $json = array(); 
 
-if(mysql_num_rows($result))
+if(mysqli_num_rows($result))
 	{
-		while($row=mysql_fetch_row($result))
+		while($row=mysqli_fetch_row($result))
 			{ 
 				$json[]=$row; 
 			} 
@@ -50,6 +40,5 @@ print( json_encode($json) );
 
 // CLOSE CONNECTION
 	mysqli_close($mysqli);
-
 
 ?>
